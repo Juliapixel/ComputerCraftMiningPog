c6fcb5a8fc4e4cf72bc30b69f913cf40aa01c394
Juliapixel <juliapasc2004@gmail.com>
2021-08-21 04:47:16 -0300

    added a simple tunneling function

--- apis/mine.lua
+++ apis/mine.lua
@@ -1,3 +1,4 @@
+
 local function oreAhead()
     local has_block, data = turtle.inspect()
     if has_block then
@@ -37,7 +38,7 @@ local function oreDown()
     end
 end
 
-function digOres()
+local function digOres()
     local up, down, left, right
     up = oreUp()
     down = oreDown()
@@ -60,3 +61,31 @@ function digOres()
     end
     turtle.turnLeft()
 end
+
+local function goForward()
+    turtle.dig()
+    turtle.forward()
+end
+
+function tunnelAhead(dist)
+    turtle.refuel()
+    if dist < turtle.getFuelLevel() then
+        dist = turtle.getFuelLevel()
+        print("not enough fuel, will only mine for %d blocks! proceed anyway? (y/n)")
+        write("> ")
+        local input = string.lower(read())
+        if input == "y" then
+            print("proceeding!")
+        else
+            print("cancelled.")
+            return(false)
+        end
+    for i=1, dist, 1 do
+        goForward()
+        digOres()
+    end
+    turtle.turnLeft()
+    turtle.turnLeft()
+    for i=1, dist, 1 do
+        goForward()
+    end
