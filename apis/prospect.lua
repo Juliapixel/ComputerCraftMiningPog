88ff6653490df8500ebb2b4aa25aa45764c42bc8
Juliapixel <juliapasc2004@gmail.com>
2021-08-21 03:26:56 -0300

    Added functions to inspect both up and down

--- apis/prospect.lua
+++ apis/prospect.lua
@@ -10,3 +10,29 @@ function oreAhead()
         return(false)
     end
 end
+
+function oreUp()
+    local has_block, data = turtle.inspectUp()
+    if has_block then
+        if string.find(textutils.serialise(data), "forge:ores") then
+            return(true)
+        else
+            return(false)
+        end
+    else
+        return(false)
+    end
+end
+
+function oreDown()
+    local has_block, data = turtle.inspectDown()
+    if has_block then
+        if string.find(textutils.serialise(data), "forge:ores") then
+            return(true)
+        else
+            return(false)
+        end
+    else
+        return(false)
+    end
+end
