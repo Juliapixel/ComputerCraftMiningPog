7ce521a457433882889ba9e4d8fd610e5bb4da0e
Juliapixel <juliapasc2004@gmail.com>
2021-08-21 03:27:54 -0300

    Added api tester for previous commit

--- apis/teste_apis.lua
+++ apis/teste_apis.lua
@@ -1,2 +1,4 @@
 os.loadAPI("ComputerCraftMiningPog/apis/prospect.lua")
 print("ore ahead: " .. tostring(prospect.oreAhead()))
+print("ore up: " .. tostring(prospect.oreUp()))
+print("ore down: " .. tostring(prospect.oreDown()))
