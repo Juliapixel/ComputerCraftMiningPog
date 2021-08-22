445ba3240cba6246938d6e1d4ca2a7a3d8889a51
Juliapixel <juliapasc2004@gmail.com>
2021-08-22 18:43:41 -0300

    fixes

--- apis/comparer/compare.lua
+++ apis/comparer/compare.lua
@@ -2,81 +2,81 @@ local itemdb = fs.open(fs.getDir(shell.getRunningProgram()) .. "/apis/comparer/i
 local items = {}
 
 while true do
-    local line = itemdb.readLine()
-    if not line then
-        itemdb.close()
-        break
-    end
-    -- #items is the ammount of items in the table, so i am adding another item to the table every time this is called
-    items[#items + 1] = line
+  local line = itemdb.readLine()
+  if not line then
+    itemdb.close()
+    break
+  end
+  -- #items is the ammount of items in the table, so i am adding another item to the table every time this is called
+  items[#items + 1] = line
 end
 
 tabulatedb(itemdb, items)
 
 function debugdb()
-    for i=1, #blocks, 1 do
-        print(blocks[i])
-    end
-    for i=1, #items, 1 do
-        print(items[i])
-    end
+  for i=1, #blocks, 1 do
+    print(blocks[i])
+  end
+  for i=1, #items, 1 do
+    print(items[i])
+  end
 end
 
 --This function will stack the items in the inventory in a way so that they will take the smallest space possible
 --will return true if last inventory slot is empty, will return false if not.
 function stackInv()
-    local inv = {}
-    for i=1, 16, 1 do
-        local data = turtle.getItemDetail(i)
-        inv[i] = data
+  local inv = {}
+  for i=1, 16, 1 do
+    local data = turtle.getItemDetail(i)
+    inv[i] = data
+  end
+  for i=1, 16 ,1 do
+    local name = inv[i]["name"]
+    for j=1, i, 1 do
+      if name == inv[j]["name"] then
+        turtle.select(i)
+        turtle.transferTo(j)
+      end
     end
-    for i=1, 16 ,1 do
-        local name = inv[i][name]
-        for j=1, i, 1 do
-            if name == inv[j][name] then
-                turtle.select(i)
-                turtle.transferTo(j)
-            end
-        end
-    end
-    turtle.select(1)
-    if turtle.getItemCount(16) < 1 then
-        return true
-    else
-        return false
-    end
+  end
+  turtle.select(1)
+  if turtle.getItemCount(16) < 1 then
+    return true
+  else
+    return false
+  end
 end
 
 --will compare the block in front of the turtle to the database of valuable blocks.
 --function compareBlock()
---    local h, data = turtle.inspect()
---    local i = 1
---    while true do
---        if not blocks[i] then
---            digDrop()
---            break
---        elseif data[name] == blocks[i] then
---            digKeep()
---            break
---        elseif data[name] ~= blocks[i] then
---            i = i + 1
---        end
---    end
+--  local h, data = turtle.inspect()
+--  local i = 1
+--  while true do
+--  if not blocks[i] then
+--    digDrop()
+--    break
+--  elseif data[name] == blocks[i] then
+--    digKeep()
+--    break
+--  elseif data[name] ~= blocks[i] then
+--    i = i + 1
+--  end
+--  end
 --end
 
 function compareInv()
-    for i=1, 16, 1 do
-        data = turtle.getItemDetail(i)
-        local j = 1
-        while true do
-            if not items[j] then
-                turtle.dropUp()
-                break
-            elseif data[name] ~= items[j] then
-                break
-            else
-                j = j + 1
-            end
-        end
+  for i=1, 16, 1 do
+    data = turtle.getItemDetail(i)
+    local j = 1
+    while true do
+      if not items[j] then
+        turtle.dropUp()
+        break
+      elseif data["name"] ~= items[j] then
+        break
+      else
+        j = j + 1
+      end
     end
+  end
 end
--- apis/mine.lua
+++ apis/mine.lua
@@ -1,99 +1,99 @@
 require("comparer.compare")
 
 local function oreAhead()
-    local has_block, data = turtle.inspect()
-    if has_block then
-        if string.find(textutils.serialise(data), "forge:ores") then
-            return true
-        else
-            return false
-        end
+  local has_block, data = turtle.inspect()
+  if has_block then
+    if string.find(textutils.serialise(data), "forge:ores") then
+      return true
     else
-        return false
+      return false
     end
+  else
+    return false
+  end
 end
 
 local function oreUp()
-    local has_block, data = turtle.inspectUp()
-    if has_block then
-        if string.find(textutils.serialise(data), "forge:ores") then
-            return true
-        else
-            return false
-        end
+  local has_block, data = turtle.inspectUp()
+  if has_block then
+    if string.find(textutils.serialise(data), "forge:ores") then
+      return true
     else
-        return false
+      return false
     end
+  else
+    return false
+  end
 end
 
 local function oreDown()
-    local has_block, data = turtle.inspectDown()
-    if has_block then
-        if string.find(textutils.serialise(data), "forge:ores") then
-            return true
-        else
-            return false
-        end
+  local has_block, data = turtle.inspectDown()
+  if has_block then
+    if string.find(textutils.serialise(data), "forge:ores") then
+      return true
     else
-        return false
+      return false
     end
+  else
+    return false
+  end
 end
 
 local function digOres()
-    local up, down, left, right
-    up = oreUp()
-    down = oreDown()
-    if up then
-        turtle.digUp()
-    end
-    if down then
-        turtle.digDown()
-    end
-    turtle.turnLeft()
-    left = oreAhead()
-    if left then 
-        turtle.dig()
-    end
-    turtle.turnRight()
-    turtle.turnRight()
-    right = oreAhead()
-    if right then
-        turtle.dig()
-    end
-    turtle.turnLeft()
+  local up, down, left, right
+  up = oreUp()
+  down = oreDown()
+  if up then
+    turtle.digUp()
+  end
+  if down then
+    turtle.digDown()
+  end
+  turtle.turnLeft()
+  left = oreAhead()
+  if left then 
+    turtle.dig()
+  end
+  turtle.turnRight()
+  turtle.turnRight()
+  right = oreAhead()
+  if right then
+    turtle.dig()
+  end
+  turtle.turnLeft()
 end
 
 local function goForward()
-    turtle.dig()
-    turtle.forward()
+  turtle.dig()
+  turtle.forward()
 end
 
 function tunnelAhead(dist)
-    dist = tonumber(dist)
-    local full_dist = 2*dist
-    if turtle.getFuelLevel() < 2 then
-        print("no fuel! cancelling...")
-        return false
+  dist = tonumber(dist)
+  local full_dist = 2*dist
+  if turtle.getFuelLevel() < 2 then
+    print("no fuel! cancelling...")
+    return false
+  end
+  if full_dist > turtle.getFuelLevel() then
+    dist = turtle.getFuelLevel()/2
+    print(string.format("not enough fuel, will only mine for %d blocks! proceed anyway? (y/n)", dist))
+    write("> ")
+    local input = string.lower(read())
+    if input == "y" then
+      print("proceeding!")
+    else
+      print("cancelled.")
+      return false
     end
-    if full_dist > turtle.getFuelLevel() then
-        dist = turtle.getFuelLevel()/2
-        print(string.format("not enough fuel, will only mine for %d blocks! proceed anyway? (y/n)", dist))
-        write("> ")
-        local input = string.lower(read())
-        if input == "y" then
-            print("proceeding!")
-        else
-            print("cancelled.")
-            return false
-        end
-    end
-    for i=1, dist, 1 do
-        goForward()
-        digOres()
-    end
-    turtle.turnLeft()
-    turtle.turnLeft()
-    for i=1, dist, 1 do
-        goForward()
-    end
+  end
+  for i=1, dist, 1 do
+    goForward()
+    digOres()
+  end
+  turtle.turnLeft()
+  turtle.turnLeft()
+  for i=1, dist, 1 do
+    goForward()
+  end
 end
