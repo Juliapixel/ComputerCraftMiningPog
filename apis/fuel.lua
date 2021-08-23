10bf72a8d3de66e304abeb38d9764dc7569ba2ae
Juliapixel <juliapasc2004@gmail.com>
2021-08-23 15:28:06 -0300

    maybe fixed fuel detection

--- apis/fuel.lua
+++ apis/fuel.lua
@@ -24,7 +24,7 @@ local function waitForFuel()
     print("please place it in the first inventory slot!")
     local event = os.pullEvent("turtle_inventory")
     local placed_fuel = turtle.getItemDetail(1)
-    if turtle.getItemDetail(1) then
+    if placed_fuel then
       --determines fuel ammount certain placed fuel item will provide. will be false if not in list.
       for i, v in ipairs(sources) do
         if v["name"] == placed_fuel["name"] then
@@ -33,7 +33,7 @@ local function waitForFuel()
           placed_fuel["power"] = false
         end
       end
-      if type(placed_fuel["power"]) == "number" then
+      if placed_fuel["power"] ~= false then
         if placed_fuel["power"]*placed_fuel["count"] <= additional_fuel_needed then
           turtle.refuel()
         elseif placed_fuel["power"]*placed_fuel["count"] > additional_fuel_needed then
