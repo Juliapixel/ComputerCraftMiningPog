a08506239ef6324dafeb7dfec1c45f975bcb82e6
Juliapixel <juliapasc2004@gmail.com>
2021-08-22 22:05:35 -0300

    fixed path to compare api and loaded fuel api

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
 args = {...}
 local mine = require("apis.mine")
-local compare = require("comparer.compare")
+local compare = require("apis.comparer.compare")
+local fuel = require("apis.fuel")
+compare.debugdb()
