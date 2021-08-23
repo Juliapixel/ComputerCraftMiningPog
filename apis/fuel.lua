a24dd28f79fa4923a1ca031b7fa7c1d8d526215e
Juliapixel <juliapasc2004@gmail.com>
2021-08-23 04:43:15 -0300

    added a function to wait for manual fueling

--- apis/fuel.lua
+++ apis/fuel.lua
@@ -1,7 +1,52 @@
 fuel = {}
+
 local mode = ""
 local totaldist = 1
-sources = {"minecraft:coal" = 80, "minecraft:coal_block"= 640, "minecraft:lava_bucket" = 1000}
+
+local coal = {name = "minecraft:coal", power = 80}
+local coal_block = {name = "minecraft:coal_block", power = 640}
+local lava_bucket = {name = "minecraft:lava_bucket, power= 100"}
+local sources = {coal, coal_block, lava_bucket}
+
+local function waitForFuel()
+  turtle.select(1)
+  local ammount_needed = 1
+  while true do
+    local additional_fuel_needed = totaldist - turtle.getFuelLevel()
+    if additional_fuel_needed <= 0 then
+      break
+    end
+    term.clear()
+    term.setCursorPos(1,1)
+    for i, item in ipairs(sources) do
+      ammount_needed = math.ceil(additional_fuel_needed/item["power"])
+      print(string.format("you  need %d more %s in order to start the turtle!", ammount_needed, string.gsub(string.gsub(item["name"], "minecraft:", ""), "_", " ")))
+    end
+    print("please place it in the first inventory slot!")
+    local event = os.pullEvent("turtle_inventory")
+    local placed_fuel = turtle.getItemDetail(1)
+    if turtle.getItemDetail(1) then
+      --determines fuel ammount certain placed fuel item will provide. will be false if not in list.
+      for i, v in ipairs(sources) do
+        if v["name"] == placed_fuel["name"] then
+          placed_fuel["power"] = v["power"]
+        elseif i == #sources then
+          placed_fuel["power"] = false
+        end
+      end
+      if type(placed_fuel["power"]) == "number" then
+        if placed_fuel["power"]*placed_fuel["count"] <= additional_fuel_needed then
+          turtle.refuel()
+        elseif placed_fuel["power"]*placed_fuel["count"] > additional_fuel_needed then
+          turtle.refuel(math.ceil(additional_fuel_needed/placed_fuel["power"]))
+        end
+      else
+        print("not a suitable fuel! try again, dumbass.")
+      end
+    end
+  end
+end
+
 --this function will decide whether the turtle should be refueled manually or automatically and for how many blocks should it refuel itself for.
 --valid modes: "manual", "auto". REQUIRES FULL PLANNED TRAVERSAL DISTANCE. MUST BE CORRECT OR SOCIETY WILL BE IN SHAMBLES.
 function fuel.refuel(varmode, dist)
@@ -11,10 +56,7 @@ function fuel.refuel(varmode, dist)
     error("not a valid mode for refueling! dumbass.")
   end
   totaldist = dist
-end
-
-local function decideAmmount()
-  
+  waitForFuel()
 end
 
 for i, line in ipairs(sources) do
