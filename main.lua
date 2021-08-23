84c9e01e6e554e8dcc81605b35061d519944fe64
Juliapixel <juliapasc2004@gmail.com>
2021-08-23 01:11:19 -0300

    whoops

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ args = {...}
 local mine = require("apis.mine")
 local compare = require("apis.comparer.compare")
 local fuel = require("apis.fuel")
-tonumber(arg[1])
+arg[1] = tonumber(arg[1])
 if type(arg[1]) ~= "number" then
   error("not a number!")
   return false
