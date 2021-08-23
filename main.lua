1ca9dcf9d0fb9a25c82316b52d8fec740ff4c6c7
Juliapixel <juliapasc2004@gmail.com>
2021-08-23 01:06:46 -0300

    made main method actually _do_ something

--- main.lua
+++ main.lua
@@ -2,5 +2,8 @@ args = {...}
 local mine = require("apis.mine")
 local compare = require("apis.comparer.compare")
 local fuel = require("apis.fuel")
-compare.stackInv()
-compare.pruneInv()
+if type(arg[1]) ~= "number" then
+  error("not a number!")
+  return false
+end
+mine.tunnelAhead(arg[1])
