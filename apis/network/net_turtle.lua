19d387533a6b7126b02e3d90e2d7be544e15537d
Juliapixel <juliapasc2004@gmail.com>
2021-08-24 19:12:06 -0300

    avoided circular require and made things safer

--- apis/network/net_turtle.lua
+++ apis/network/net_turtle.lua
@@ -1,12 +1,10 @@
-local mine = require("apis.mine")
-
 netTurtle = {}
 
 local worker_info = {
   ID = os.getComputerID(),
   name = os.getComputerLabel(),
   role = "miner",
-  progress = mine.progress(),
+  progress = 0,
   present = true,
   curTask = ""
 }
@@ -25,7 +23,22 @@ local function waitForCommand()
   end
 end
 
+local function isForbiddenName(name)
+  local forbiddenNames = {ID, name, present}
+  return forbiddenNames[name] ~= nil
+end
+
+-- updates the worker_info table to send up to date info to receivers
+-- available names: 
+function netTurtle.updateInfo(name, value)
+  if isForbiddenName then
+    error("tried to change forbidden value in turtle info!", 2)
+  end
+  worker_info[name] = value
+end
+
 function netTurtle.run()
   waitForCommand()
 end
+
 return netTurtle
