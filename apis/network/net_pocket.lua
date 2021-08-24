f89e59328a0b2b8d2645fb6ff9d4f460384d4a0e
Juliapixel <juliapasc2004@gmail.com>
2021-08-24 17:12:07 -0300

    try to fix updateAll()

--- apis/network/net_pocket.lua
+++ apis/network/net_pocket.lua
@@ -45,8 +45,9 @@ end
 local function updateAll()
   os.startTimer(5)
   for i = 1, initialAmmount do
-    if worker_info[i] then 
-      updateInfo(worker_info[i]["ID"])
+    if worker_info[i] then
+      local requestID = worker_info[i]["ID"]
+      updateInfo(requestID)
     end
   end
   event = os.pullEvent(timer)
