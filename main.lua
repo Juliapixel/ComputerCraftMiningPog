1a64fac82c86b4eba5c38350fbf285209c283b4a
Juliapixel <juliapasc2004@gmail.com>
2021-08-25 02:15:52 -0300

    now modems are closed when done

--- main.lua
+++ main.lua
@@ -3,8 +3,21 @@ local mine = require("apis.mine")
 local compare = require("apis.comparer.compare")
 local fuel = require("apis.fuel")
 arg[1] = tonumber(arg[1])
-if type(arg[1]) ~= "number" then
-  error("not a number!")
-  return false
+-- tells the turtle to mine according to received arguments and runs its networking functions in parallel
+if turtle then
+  if type(arg[1]) ~= "number" then
+    error("not a number!")
+    return false
+  end
+  local function wrapper()
+    mine.tunnelAhead(args[1])
+  end
+  parallel.waitForAny(net.run, wrapper)
+  rednet.close()
+end
+-- will simply work as a tool to monitor turtles
+if pocket then
+  net.run()
+  rednet.close()
 end
 mine.tunnelAhead(arg[1])
