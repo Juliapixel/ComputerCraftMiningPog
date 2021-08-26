8fa51e4c49ecb18330b0ec58256dc40d4dca590f
Juliapixel <juliapasc2004@gmail.com>
2021-08-26 03:27:24 -0300

    updated to be cleaner

--- main.lua
+++ main.lua
@@ -3,15 +3,15 @@ local net = require("apis.network.net")
 local mine = require("apis.mine")
 local compare = require("apis.comparer.compare")
 local fuel = require("apis.fuel")
-arg[1] = tonumber(arg[1])
+local mine_dist = tonumber(args[1])
 -- tells the turtle to mine according to received arguments and runs its networking functions in parallel
 if turtle then
-  if type(arg[1]) ~= "number" then
+  if type(mine_dist) ~= "number" then
     error("not a number!")
     return false
   end
   local function wrapper()
-    mine.tunnelAhead(args[1])
+    mine.tunnelAhead(mine_dist)
   end
   parallel.waitForAny(net.run, wrapper)
   rednet.close()
